3a31904d1d209755a3de4e9c3cbd878da36df73c
Gilsing,Guylian Gilsing G <g.gilsing@student.fontys.nl>
2023-03-18 14:36:27 +0100

    run action

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ use function GuylianGilsingWebsite\APIs\Application\get_application_instance;
 require_once __DIR__.'/../src/bootstrap.php';
 
 echo '<pre>';
-var_dump($_SERVER);
+var_dump($_SERVER); 
 echo '</pre>';
 
 get_application_instance()->run();
