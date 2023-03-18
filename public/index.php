74378e6596fc569c258122b4e1711eca711ae7f8
Gilsing,Guylian Gilsing G <g.gilsing@student.fontys.nl>
2023-03-18 15:28:38 +0100

    Removed logging

--- public/index.php
+++ public/index.php
@@ -7,9 +7,5 @@ use function GuylianGilsingWebsite\APIs\Application\get_application_instance;
 
 require_once __DIR__.'/../src/bootstrap.php';
 
-echo '<pre>';
-var_dump($_SERVER);
-echo '</pre>';
-
 get_application_instance()->run();
 application_cleanup();
